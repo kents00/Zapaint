5a1069bebad3dd6b9cad4cebc39be2d4672e7c39
Kent Edoloverio <69900896+kents00@users.noreply.github.com>
2023-09-17 15:21:32 +0800

    fix unwanted typos

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ from bpy.types import Panel, Operator
 
 class Zapaint_op_Materials(Operator):
     bl_label = ""
-    bl_idname = "zapaint.op.materials"
+    bl_idname = "zapaint.op_materials"
 
     @classmethod
     def poll(cls,context):
@@ -32,7 +32,7 @@ class Zapaint_op_Materials(Operator):
 
 class Zapaint_op_Node(Operator):
     bl_label = ""
-    bl_idname = "zapaint.op.node"
+    bl_idname = "zapaint.op_node"
 
     @classmethod
     def poll(cls,context):
@@ -43,7 +43,7 @@ class Zapaint_op_Node(Operator):
 
 class Zapaint_op_Brush(Operator):
     bl_label = ""
-    bl_idname = "zapaint.op.brush"
+    bl_idname = "zapaint.op_brush"
 
     @classmethod
     def poll(cls,context):
@@ -54,7 +54,7 @@ class Zapaint_op_Brush(Operator):
 
 class Zapaint_op_Palettes(Operator):
     bl_label = ""
-    bl_idname = "zapaint.op.palettes"
+    bl_idname = "zapaint.op_palettes"
 
     @classmethod
     def poll(cls,context):
@@ -65,7 +65,7 @@ class Zapaint_op_Palettes(Operator):
 
 class Zapaint_op_Layers(Operator):
     bl_label = ""
-    bl_idname = "zapaint.op.layers"
+    bl_idname = "zapaint.op_layers"
 
     @classmethod
     def poll(cls,context):
@@ -74,58 +74,57 @@ class Zapaint_op_Layers(Operator):
     def execute(self,context):
         pass
 
-class Zapaint_pl_Base:
-    bl_idname = "Zapaint_pl_Base"
+class Zapaint_UI(Panel):
+    bl_label = "Zapaint"
     bl_space_type = "VIEW_3D"
-    bl_region_type = 'TOOLS' if bpy.app.version < (2, 80) else 'UI'
+    bl_region_type = 'UI'
     bl_options = {'HEADER_LAYOUT_EXPAND'}
-    bl_category = "PiXel"
-    bl_order = 0
-    bl_ui_units_x=1
+    bl_category = "Zapaint"
 
     @classmethod
     def poll(cls, context):
         return context.scene.render.engine == 'BLENDER_EEVEE'
 
-    def draw(self,context):
-        if context.active_object and context.active_object.type != 'MESH':
+    def draw(self, context):
+        active_object = context.active_object
+        if active_object and active_object.type != 'MESH':
             layout = self.layout.box()
             row = layout.row()
-            row.label(text="Select an Object", icon="ERROR")
+            row.label(text="Select an MESH Object", icon="ERROR")
 
-class Zapaint_pl_Materials(Zapaint_pl_Base,Panel):
-    bl_parent_id = "PiXel_pl_Base"
+class Zapaint_pl_Materials(Panel):
     bl_label = "Materials"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
+    bl_category = "Zapaint"
+    bl_parent_id = "Zapaint_UI"
     bl_context = ''
-    bl_order = 1
 
     @classmethod
-    def poll(cls, context):
-        return not False
+    def poll(cls,context):
+        pass
 
     def draw(self, context):
-        _object = context.active__object
+        ob = context.active_object
 
-        if _object and _object.type == 'MESH':
+        if ob and ob.type == 'MESH':
             layout = self.layout.box()
             row = layout.row()
-            row.template_list("MATERIAL_UL_matslots", "", _object, "material_slots", _object, "active_material_index", rows=5)
-            is_sortable = len(_object.material_slots) > 1
+            row.template_list("MATERIAL_UL_matslots", "", ob, "material_slots", ob, "active_material_index", rows=5)
+            is_sortable = len(ob.material_slots) > 1
             col = row.column(align=True)
-            col.operator("_object.material_slot_add", icon='ADD', text="")
-            col.operator("_object.material_slot_remove", icon='REMOVE', text="")
+            col.operator("object.material_slot_add", icon='ADD', text="")
+            col.operator("object.material_slot_remove", icon='REMOVE', text="")
             col.separator()
             col.menu("MATERIAL_MT_context_menu", icon='DOWNARROW_HLT', text="")
             if is_sortable:
                 col.separator()
-                col.operator("_object.material_slot_move", icon='TRIA_UP', text="").direction = 'UP'
-                col.operator("_object.material_slot_move", icon='TRIA_DOWN', text="").direction = 'DOWN'
+                col.operator("object.material_slot_move", icon='TRIA_UP', text="").direction = 'UP'
+                col.operator("object.material_slot_move", icon='TRIA_DOWN', text="").direction = 'DOWN'
             row = layout.row()
-            row.template_ID(_object, "active_material", new="material.new")
+            row.template_ID(ob, "active_material", new="material.new")
 
-            if _object.mode == 'EDIT':
+            if ob.mode == 'EDIT':
                 row = layout.row(align=True)
                 row.enabled = False
                 row.enabled = True
@@ -133,71 +132,79 @@ class Zapaint_pl_Materials(Zapaint_pl_Base,Panel):
                 row.operator("object.material_slot_select", text="Select")
                 row.operator("object.material_slot_deselect", text="Deselect")
 
-class Zapaint_pl_Node(Zapaint_pl_Base,Panel):
-    bl_parent_id = "Zapaint_pl_Base"
-    bl_label = "Shaders"
+class Zapaint_pl_Node(Panel):
+    bl_label = "Node"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
+    bl_category = "Zapaint"
+    bl_parent_id = "Zapaint_UI"
     bl_context = ''
-    bl_order = 2
 
     @classmethod
-    def poll(cls, context):
-        return not False
-
-    def draw(self, context):
+    def poll(cls,context):
         pass
-class Zapaint_pl_Brush(Zapaint_pl_Base,Panel):
-    bl_parent_id = "Zapaint_pl_Base"
+
+    def draw(self,context):
+        pass
+
+class Zapaint_pl_Brush(Panel):
     bl_label = "Brush"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
+    bl_category = "Zapaint"
+    bl_parent_id = "Zapaint_UI"
     bl_context = ''
-    bl_order = 3
 
     @classmethod
-    def poll(cls, context):
-        return not False
-
-    def draw(self, context):
+    def poll(cls,context):
         pass
 
-class Zapaint_pl_Palettes(Zapaint_pl_Base,Panel):
-    bl_parent_id = "Zapaint_pl_Base"
-    bl_label = "Palettes"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_context = ''
-    bl_order = 4
-
-    @classmethod
-    def poll(cls, context):
-        return not False
-
-    def draw(self, context):
+    def draw(self,context):
         pass
 
-class Zapaint_pl_Layers(Zapaint_pl_Base,Panel):
-    bl_parent_id = "Zapaint_pl_Base"
+class Zapaint_pl_Layers(Panel):
     bl_label = "Layers"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
+    bl_category = "Zapaint"
+    bl_parent_id = "Zapaint_UI"
     bl_context = ''
-    bl_order = 5
 
     @classmethod
-    def poll(cls, context):
-        return not False
-
-    def draw(self, context):
+    def poll(cls,context):
         pass
 
+    def draw(self,context):
+        pass
+
+class Zapaint_pl_Palettes(Panel):
+    bl_label = "Palettes"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Zapaint"
+    bl_parent_id = "Zapaint_UI"
+    bl_context = ''
+
+    @classmethod
+    def poll(cls,context):
+        pass
+
+    def draw(self,context):
+        pass
+
+
 classes = (
-    Zapaint_pl_Base,
-    Zapaint_pl_Layers,
+    Zapaint_UI,
+    Zapaint_pl_Materials,
     Zapaint_pl_Node,
-    Zapaint_pl_Palettes,
+    Zapaint_pl_Brush,
     Zapaint_pl_Layers,
+    Zapaint_pl_Palettes,
+    Zapaint_op_Materials,
+    Zapaint_op_Node,
+    Zapaint_op_Brush,
+    Zapaint_op_Layers,
+    Zapaint_op_Palettes,
 )
 
 def register():

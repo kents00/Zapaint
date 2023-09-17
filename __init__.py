8b44497b5f6bd37a5b5458942ad07ef79e796c86
Kent Edoloverio <69900896+kents00@users.noreply.github.com>
2023-09-17 11:24:31 +0800

    fix unwanted typos

--- __init__.py
+++ __init__.py
@@ -21,9 +21,13 @@ class Zapaint_op_Materials(Operator):
 
     @classmethod
     def poll(cls,context):
-        pass
+        if context.active_object and context.active_object.type == 'MESH':
+            if not context.active_object.data.materials:
+                return True
+        return False
 
     def execute(self,context):
+
         pass
 
 class Zapaint_op_Node(Operator):
@@ -83,6 +87,12 @@ class Zapaint_pl_Base:
     def poll(cls, context):
         return context.scene.render.engine == 'BLENDER_EEVEE'
 
+    def draw(self,context):
+        if context.active_object and context.active_object.type != 'MESH':
+            layout = self.layout.box()
+            row = layout.row()
+            row.label(text="Select an Object", icon="ERROR")
+
 class Zapaint_pl_Materials(Zapaint_pl_Base,Panel):
     bl_parent_id = "PiXel_pl_Base"
     bl_label = "Materials"
@@ -96,10 +106,35 @@ class Zapaint_pl_Materials(Zapaint_pl_Base,Panel):
         return not False
 
     def draw(self, context):
-        pass
+        _object = context.active__object
+
+        if _object and _object.type == 'MESH':
+            layout = self.layout.box()
+            row = layout.row()
+            row.template_list("MATERIAL_UL_matslots", "", _object, "material_slots", _object, "active_material_index", rows=5)
+            is_sortable = len(_object.material_slots) > 1
+            col = row.column(align=True)
+            col.operator("_object.material_slot_add", icon='ADD', text="")
+            col.operator("_object.material_slot_remove", icon='REMOVE', text="")
+            col.separator()
+            col.menu("MATERIAL_MT_context_menu", icon='DOWNARROW_HLT', text="")
+            if is_sortable:
+                col.separator()
+                col.operator("_object.material_slot_move", icon='TRIA_UP', text="").direction = 'UP'
+                col.operator("_object.material_slot_move", icon='TRIA_DOWN', text="").direction = 'DOWN'
+            row = layout.row()
+            row.template_ID(_object, "active_material", new="material.new")
+
+            if _object.mode == 'EDIT':
+                row = layout.row(align=True)
+                row.enabled = False
+                row.enabled = True
+                row.operator("object.material_slot_assign", text="Assign")
+                row.operator("object.material_slot_select", text="Select")
+                row.operator("object.material_slot_deselect", text="Deselect")
 
 class Zapaint_pl_Node(Zapaint_pl_Base,Panel):
-    bl_parent_id = "PiXel_pl_Base"
+    bl_parent_id = "Zapaint_pl_Base"
     bl_label = "Shaders"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -113,7 +148,7 @@ class Zapaint_pl_Node(Zapaint_pl_Base,Panel):
     def draw(self, context):
         pass
 class Zapaint_pl_Brush(Zapaint_pl_Base,Panel):
-    bl_parent_id = "PiXel_pl_Base"
+    bl_parent_id = "Zapaint_pl_Base"
     bl_label = "Brush"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -128,7 +163,7 @@ class Zapaint_pl_Brush(Zapaint_pl_Base,Panel):
         pass
 
 class Zapaint_pl_Palettes(Zapaint_pl_Base,Panel):
-    bl_parent_id = "PiXel_pl_Base"
+    bl_parent_id = "Zapaint_pl_Base"
     bl_label = "Palettes"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -143,7 +178,7 @@ class Zapaint_pl_Palettes(Zapaint_pl_Base,Panel):
         pass
 
 class Zapaint_pl_Layers(Zapaint_pl_Base,Panel):
-    bl_parent_id = "PiXel_pl_Base"
+    bl_parent_id = "Zapaint_pl_Base"
     bl_label = "Layers"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'

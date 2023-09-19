722b74efb72ae4f29500f276460451dc12bc5969
Kent Edoloverio <69900896+kents00@users.noreply.github.com>
2023-09-19 11:30:15 +0800

    adding materials section

--- __init__.py
+++ __init__.py
@@ -39,8 +39,32 @@ class Zapaint_pl_Materials(Zapaint_UI, bpy.types.Panel):
     bl_label = "Materials"
 
     def draw(self, context):
-        layout = self.layout
-        layout.label(text="First Sub Panel of Panel 1.")
+        ob = context.active_object
+
+        if ob and ob.type == 'MESH':
+            layout = self.layout.box()
+            row = layout.row()
+            row.template_list("MATERIAL_UL_matslots", "", ob, "material_slots", ob, "active_material_index", rows=5)
+            is_sortable = len(ob.material_slots) > 1
+            col = row.column(align=True)
+            col.operator("object.material_slot_add", icon='ADD', text="")
+            col.operator("object.material_slot_remove", icon='REMOVE', text="")
+            col.separator()
+            col.menu("MATERIAL_MT_context_menu", icon='DOWNARROW_HLT', text="")
+            if is_sortable:
+                col.separator()
+                col.operator("object.material_slot_move", icon='TRIA_UP', text="").direction = 'UP'
+                col.operator("object.material_slot_move", icon='TRIA_DOWN', text="").direction = 'DOWN'
+            row = layout.row()
+            row.template_ID(ob, "active_material", new="material.new")
+
+            if ob.mode == 'EDIT':
+                row = layout.row(align=True)
+                row.enabled = False
+                row.enabled = True
+                row.operator("object.material_slot_assign", text="Assign")
+                row.operator("object.material_slot_select", text="Select")
+                row.operator("object.material_slot_deselect", text="Deselect")
 
 class Zapaint_pl_Nodes(Zapaint_UI, bpy.types.Panel):
     bl_idname = "Zapaint_pl_Nodes"
